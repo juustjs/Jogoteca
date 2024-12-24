12285c37d74b9982e7c78a693068a744010216a2
juustjs <julianasalgado47@gmail.com>
2024-12-24 18:47:33 -0400

    Criando um novo jogo e aplicando request no python

--- jogoteca.py
+++ jogoteca.py
@@ -1,6 +1,8 @@
 #importando o Flask 
-#importando render template para gerenciar o front com html
-from flask import Flask, render_template
+#importando render template para gerenciar com html
+from flask import Flask
+from flask import render_template
+from flask import request
 
 #criando uma classe 
 class Jogo:
@@ -8,19 +10,36 @@ class Jogo:
         self._nome = nome
         self._categoria = categoria
         self._console = console
+
+jogo1 = Jogo('The Sims', 'Sandbox', 'pc')
+jogo2 = Jogo('valorant','Tiro','xbox')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'Ps5')
+    
+lista =[jogo1, jogo2, jogo3]
         
 #chamando a aplicação
 app = Flask(__name__)
 
 #criando rota inicio
-@app.route('/Inicio')
-def ola():
-    jogo1 = Jogo('The Sims', 'Sandbox', 'pc')
-    jogo2 = Jogo('valorant','Tiro','xbox')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'Ps5')
-    
-    lista =[jogo1, jogo2, jogo3]
+@app.route('/')
+def index():
     return render_template('lista.html', titulo ='Jogos', jogos=lista)
 
+#criando uma nova rota para cadastrar jogos direto no html
+@app.route('/cadastro')
+def cadastro():
+    return render_template('novo.html', titulo='Cadastrar Novo Jogo')
+
+#criando uma nova rota request para receber os dados cadastrados vindo do html
+@app.route('/criar', methods=['POST',])
+def criar ():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo =  Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
 #rodando a aplicação
-app.run() 
+#ativando o debug
+app.run(debug=True) 
