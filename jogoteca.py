7e69bd87dff5beb16b9af9b97c29eafbabe5c79e
juustjs <julianasalgado47@gmail.com>
2024-12-24 16:50:13 -0400

    comentando linhas e refatorando script

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,26 @@
+#importando o Flask 
+#importando render template para gerenciar o front com html
 from flask import Flask, render_template
 
+#criando uma classe 
+class Jogo:
+    def __init__(self,nome,categoria,console):
+        self._nome = nome
+        self._categoria = categoria
+        self._console = console
+        
+#chamando a aplicação
 app = Flask(__name__)
 
+#criando rota inicio
 @app.route('/Inicio')
 def ola():
-    jogos = ['The sims', 'Valorant', 'Crash']
-    return render_template('lista.html', titulo='Jogos')
+    jogo1 = Jogo('The Sims', 'Sandbox', 'pc')
+    jogo2 = Jogo('valorant','Tiro','xbox')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'Ps5')
+    
+    lista =[jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo ='Jogos', jogos=lista)
 
-
-app.run()
+#rodando a aplicação
+app.run() 
